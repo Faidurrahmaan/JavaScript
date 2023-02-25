2f3821fb3f4008b1454e4a1e30dd6078e5022458
faidur <abiodunolamilekan19992004@gmail.com>
2023-02-25 12:16:05 +0100

    first commit

---
+++ exercise.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+add(2,2);
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+subtract(2,2);
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+multiply(2,2);
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
+divide(2,2);
